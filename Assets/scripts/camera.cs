1001fb89e71aa568a063a8705751ed03a64855f4
professorDODO <roman.pollei@gmail.com>
2017-09-10 04:20:23 +0200

    a fresh start

---
+++ Assets/scripts/camera.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XboxCtrlrInput;
+
+public class camera : MonoBehaviour {
+
+	public Transform P;
+	public bool invertX;
+	public bool invertY;
+	public float camZoomAccuracy;
+	public float zoomOutOffset;
+	public float zoomInOffset;
+	public float camSpeed;
+	public float minDistance;
+	private float FoVZoomIn;
+	private float FoVZoomOut;
+
+	void Awake() {
+		FoVZoomIn = Camera.main.fieldOfView/Camera.main.aspect - zoomInOffset;
+		FoVZoomOut = Camera.main.fieldOfView/Camera.main.aspect - zoomOutOffset;
+	}
+
+	void Update() {
+		//position of players
+		Vector3[] pLoc = new Vector3[P.childCount];
+		for (int i = 0; i < P.childCount; ++i) {
+			pLoc[i] = P.GetChild(i).position;
+		}
+		// midpos of all Players
+		Vector3 middle = transform.parent.position;
+
+		//turning camera
+		float rsXInput = 0;
+		float rsYInput = 0;
+		for(int i = 1; i <= pLoc.Length; i++) {
+			
+			rsXInput += (invertX ? -1 : 1) * XCI.GetAxis (XboxAxis.RightStickX, (XboxController)i);
+			rsYInput += (invertY ? 1 : -1) * XCI.GetAxis (XboxAxis.RightStickY, (XboxController)i);
+		}
+
+		transform.LookAt(middle);
+		if (transform.rotation.eulerAngles.x < 88) {
+			transform.RotateAround (middle, (rsXInput * Vector3.up + rsYInput * transform.right), Time.deltaTime * camSpeed);
+		} else if (transform.rotation.eulerAngles.x > 88 && transform.rotation.eulerAngles.x < 270 && rsYInput <= 0) {
+			transform.RotateAround (middle, (rsXInput * Vector3.up + rsYInput * transform.right), Time.deltaTime * camSpeed);
+		} else if (transform.rotation.eulerAngles.x > 270 && rsYInput >= 0) {
+			transform.RotateAround (middle, (rsXInput * Vector3.up + rsYInput * transform.right), Time.deltaTime * camSpeed);
+		}
+
+		//zoom out
+		for (int i = 0; i < pLoc.Length; i++) {
+			//checking if a player is outside of the view of the camera
+			if (Vector3.Angle((pLoc[i] - transform.position), (middle - transform.position)) > FoVZoomOut) {
+				transform.LookAt (middle);
+				transform.position -= transform.forward.normalized * (pLoc [i] - transform.position).magnitude
+					* Mathf.Sin (Mathf.PI/180*(Vector3.Angle((pLoc [i] - transform.position), (middle - transform.position)) - FoVZoomOut))
+					/ Mathf.Sin (Mathf.PI/180*FoVZoomOut);
+			}
+		}
+
+		//zoom in
+		float maxAngle = 0;
+		int playerMaxAngleIndex = -1;
+		for (int i = 0; i < P.childCount; ++i) {
+			if (Vector3.Angle ((pLoc [i] - transform.position), (middle - transform.position)) > maxAngle) {
+				maxAngle = Vector3.Angle ((pLoc [i] - transform.position), (middle - transform.position));
+				playerMaxAngleIndex = i;
+			}
+			if (P.childCount == 1) playerMaxAngleIndex = 0;
+		}
+		if (maxAngle < FoVZoomIn) {
+			transform.LookAt (middle);
+			transform.position -= transform.forward.normalized * (pLoc [playerMaxAngleIndex] - transform.position).magnitude
+				* Mathf.Sin (Mathf.PI/180*(Vector3.Angle((pLoc [playerMaxAngleIndex] - transform.position), (middle - transform.position)) - FoVZoomIn))
+				/ Mathf.Sin (Mathf.PI/180*FoVZoomIn);
+		}
+
+		// set min distance to nearest Player
+		for (int i = 0; i < pLoc.Length; i++) {
+			if ((pLoc [i] - transform.position).magnitude < minDistance) {
+				transform.LookAt(middle);
+				transform.position -= transform.forward.normalized * (minDistance * Mathf.Sqrt (1 -
+									Mathf.Pow ((pLoc [i] - transform.position).magnitude / minDistance
+									* Mathf.Sin (Mathf.PI / 180 * Vector3.Angle ((pLoc [i] - transform.position), (middle - transform.position))), 2))
+									- (pLoc [i] - transform.position).magnitude * Mathf.Cos (Mathf.PI / 180 * Vector3.Angle ((pLoc [i] - transform.position), (middle - transform.position))));
+			}
+		}
+
+		//focus camera at mid point between players
+		transform.LookAt(middle);
+	}
+}
---
+++ Assets/scripts/middleLoc.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class middleLoc : MonoBehaviour {
+
+	public Transform P;
+	[HideInInspector]public Vector3 camForward;
+	[HideInInspector]public Vector3 camRight;
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3[] pLoc = new Vector3[P.childCount];
+		for (int i = 0; i < P.childCount; ++i) {
+			pLoc[i] = P.GetChild(i).position;
+		}
+		// midpos of all Players
+		Vector3 middle = new Vector3(0,0,0);
+		for (int i = 0; i < pLoc.Length; i++) {
+			middle += pLoc [i];
+		}
+		middle = middle / pLoc.Length;
+		transform.position = middle;
+	}
+}
