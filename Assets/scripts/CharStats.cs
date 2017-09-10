7ebc4a483d80b83f4ad0018291be825011e7f65f
Myzos <nikolai.richter@gmx.de>
2017-09-10 18:38:28 +0200

    CharacterStats added

---
+++ Assets/scripts/CharStats.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharStats : MonoBehaviour {
+
+	// ENUMS
+
+	public enum TYPE{
+		PLAYER,
+		ENEMY,
+		OBJECT
+	};
+
+	public enum STATS{
+		HEALTH,
+		PAIN,
+		STRENGTH,
+		SPEED,
+	};
+
+	// STATS
+
+	public TYPE type;
+	public int playerNumber;
+
+
+	// default stat variables
+	int[] defStats = new int[4];	// adjust array length if needed
+
+	// dynamic stat variables
+	[HideInInspector]
+	public float[] dynStats = new float[4];	// adjust array length if needed
+
+	// METHODS
+	public void StatChange(STATS stat, float change){
+		dynStats [(int)stat] += change;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//StatChange (STATS.HEALTH, 2);
+		Debug.Log (dynStats [(int)STATS.PAIN]);
+	}
+}
---
+++ Assets/scripts/Test_StatChange.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_StatChange : MonoBehaviour {
+
+	public GameObject character;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		character.GetComponent<CharStats> ().StatChange (CharStats.STATS.PAIN, 1);
+	}
+}
