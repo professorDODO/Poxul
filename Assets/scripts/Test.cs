b032a2f59c46565e2d34af11513804c17c782f63
unknown <dietrich.leonard@googlemail.com>
2017-09-12 05:55:40 +0200

    queue und so

---
+++ Assets/scripts/Test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	void Start () {
+		// print(queue.to_array());
+		Action_Q queue = new Action_Q();
+		queue.print_queue();
+		
+		Action walk = new Action("walk", 0.2f);
+		Action stab = new Action("stab", 3.0f);
+		queue.enqueue(walk, 0.5f);
+		queue.print_queue();
+		queue.enqueue(stab, 2.8f);
+		queue.print_queue();
+
+	}
+
+
+}
