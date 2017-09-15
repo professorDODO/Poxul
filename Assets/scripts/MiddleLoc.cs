9e8bc9baf14302e5505485da428bc28af1cb12ea
professorDODO <roman.pollei@gmail.com>
2017-09-15 14:39:05 +0200

    configure git for case sensitivity changes

---
+++ Assets/scripts/MiddleLoc.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiddleLoc : MonoBehaviour {
+
+	public Transform Player;
+
+	void Update () {
+		// midpos of all Players
+		Vector3[] pLoc = Player.GetComponent<PlayerLocation>().pLoc;
+		Vector3 middle = new Vector3(0,0,0);
+		for (int i = 0; i < pLoc.Length; i++) {
+			middle += pLoc [i];
+		}
+		middle = middle / pLoc.Length;
+		transform.position = middle;
+	}
+}
