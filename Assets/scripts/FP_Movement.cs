c59aa2586d9d01bda360e256a2efc2899388539d
unknown <dietrich.leonard@googlemail.com>
2017-09-18 21:21:39 +0200

    n lev

---
+++ Assets/scripts/FP_Movement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XboxCtrlrInput;
+
+
+public class FP_Movement : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float playerIndex = 1;
+	public float groundDrag;
+	public float moveForce = 50;
+	public float maxSpeed = 10;
+	
+	void Awake() { rb = GetComponent<Rigidbody>(); }
+
+
+	void Update() {
+		Vector3 forwardDir = new Vector3 (0,0,1);
+		Vector3 rightDir = new Vector3 (1,0,0);
+		float lsX = XCI.GetAxis (XboxAxis.RightStickX, (XboxController)playerIndex);
+		float lsY = XCI.GetAxis (XboxAxis.RightStickY, (XboxController)playerIndex);
+
+		if (XCI.GetAxis(XboxAxis.LeftStickX, (XboxController)playerIndex) == 0 && XCI.GetAxis(XboxAxis.LeftStickY, (XboxController)playerIndex) == 0) {
+			rb.velocity = new Vector3 (rb.velocity.x * groundDrag, rb.velocity.y, rb.velocity.z * groundDrag);
+		} else {
+			rb.AddForce (rightDir * lsX * moveForce / groundDrag);
+			rb.AddForce (forwardDir * lsY * moveForce / groundDrag);
+		}
+		if (rb.velocity.magnitude > maxSpeed) {
+			//future: addForce
+			float yVel = rb.velocity.y;
+			rb.velocity = rb.velocity.normalized * maxSpeed;
+			rb.velocity = new Vector3 (rb.velocity.x, yVel,rb.velocity.z);
+		}
+		if (lsX != 0 && lsY != 0) {
+			transform.rotation = Quaternion.LookRotation (rightDir * lsX + forwardDir * lsY);
+		}
+	}
+}
---
+++ Assets/scripts/Turn.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turn : MonoBehaviour {
+
+	public Transform Player;
+	public Transform Enemy;
+	public Transform Ghosts;
+
+	void Awake () {
+		for (int i = 0; i < childCount (Player); i++) {
+			GameObject a = new GameObject ("Ghost_" + i);
+			a.transform.SetParent(Ghosts);
+			a.transform.position = Player.GetChild(i).position;
+			print (Player.GetChild (i).GetComponent<CharStats> ());
+
+			//a.AddComponent<CharStats>(Player.GetChild (i).GetComponent<CharStats>());
+			CharStats pff = a.AddComponent<CharStats> () as CharStats;
+			//pff = Player.GetChild (i).GetComponent<CharStats> ();
+			//pff.playerNumber = i;
+		}
+	}
+		
+	public int childCount(Transform Player) {
+		int childCount = 0;
+		for (int i = 0; i < Player.childCount; i++) {
+			if(Player.GetChild(i).gameObject.activeSelf) {
+				childCount++;
+			}
+		}
+		return childCount;
+	}
+
+
+
+	void Update () {
+
+	}
+}
