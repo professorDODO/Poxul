86d29553a0307a3bd66dfc5432e2593ef75a7619
Myzos <nikolai.richter@gmx.de>
2017-09-23 20:29:36 +0200

    Freeze added

--- Assets/scripts/MovementVelo.cs
+++ Assets/scripts/MovementVelo.cs
@@ -10,7 +10,6 @@ public class MovementVelo : MonoBehaviour {
 	// rb. += turning * transform.forward * acceleration;
 
 	private Rigidbody rb;
-	public GameObject cam;
 	public float playerIndex;
 	public float groundDrag;
 	public float acceleration;
@@ -18,16 +17,10 @@ public class MovementVelo : MonoBehaviour {
 
 	float lsX;			// LeftStickX
 	float lsY;			// LeftStickY
-	//Vector3 dir;
-	//Vector3 forwardDir;
-	//Vector3 rightDir;
-
-	void SetDirection(float axis, Vector3 camDir){
-		float vel = rb.velocity.magnitude * Mathf.Cos (Vector3.Angle (rb.velocity, camDir) * Mathf.PI / 180);
-		if(axis < 0 && vel > -maxSpeed || axis > 0 && vel < maxSpeed){
-			rb.velocity += camDir * axis * acceleration * Time.deltaTime / groundDrag;
-		}
-	}
+	Vector3 inDir;
+	Vector3 rightDir;
+	Vector3 forwardDir;
+	Vector3 modInDir;
 
 	void Awake() {
 		rb = GetComponent<Rigidbody>();
@@ -36,25 +29,18 @@ public class MovementVelo : MonoBehaviour {
 	void Update() {
 		lsX = XCI.GetAxis (XboxAxis.LeftStickX, (XboxController)playerIndex);
 		lsY = XCI.GetAxis (XboxAxis.LeftStickY, (XboxController)playerIndex);
-		//forwardDir = new Vector3 (Camera.main.transform.forward.x, 0, Camera.main.transform.forward.z).normalized;
-		//rightDir = new Vector3 (Camera.main.transform.right.x, 0, Camera.main.transform.right.z).normalized;
+		inDir = new Vector3 (lsX, 0, lsY);
+		// enables movement relative to the camera angle
+		rightDir = new Vector3 (Camera.main.transform.right.x, 0, Camera.main.transform.right.z).normalized;
+		forwardDir = new Vector3 (Camera.main.transform.forward.x, 0, Camera.main.transform.forward.z).normalized;
+		modInDir = Quaternion.Euler (0, Camera.main.transform.eulerAngles.y, 0) * inDir;
 		if (lsX == 0 && lsY == 0) {
 			rb.velocity = new Vector3 (rb.velocity.x * groundDrag, rb.velocity.y, rb.velocity.z * groundDrag);
-		} else {
-			Quaternion turning = Quaternion.Euler (0, cam.transform.eulerAngles.y, 0);
-			Vector3 curVelo = cam.transform.InverseTransformVector (rb.velocity);
-			if (lsX < 0 && curVelo.x > -maxSpeed || lsX > 0 && curVelo.x < maxSpeed) {
-				rb.velocity += turning * transform.right * lsX * acceleration * Time.deltaTime / groundDrag;
-			}
-			if (lsY < 0 && curVelo.z > -maxSpeed || lsY > 0 && curVelo.z < maxSpeed) {
-				rb.velocity += turning * transform.forward * lsY * acceleration * Time.deltaTime / groundDrag;
-			}
-			//dir = Vector3.zero;
-			//SetDirection (lsX, rightDir);
-			//SetDirection (lsY, forwardDir);
-			//Debug.Log (dir);
-			//rb.velocity += dir.normalized * acceleration * Time.deltaTime / groundDrag;
 		}
-		//Debug.Log (maxSpeed - rb.velocity.magnitude);
+		if (Mathf.Abs (rb.velocity.magnitude * Mathf.Cos (Vector3.Angle (rb.velocity, modInDir) * Mathf.PI / 180)) >= maxSpeed) {
+			inDir = Vector3.zero;
+		}
+		rb.velocity += modInDir * acceleration / groundDrag * Time.deltaTime;
+		Debug.Log (rb.velocity.magnitude * Mathf.Cos (Vector3.Angle (rb.velocity, modInDir) * Mathf.PI / 180));
 	}
 }
